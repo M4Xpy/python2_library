0d9ed10835b8354749509374134956350e964f09
Maxim V <m4k51m15k4m@gmail.com>
2024-06-21 10:20:25 +0300

    update settings.py

--- library_system/library_system/settings.py
+++ library_system/library_system/settings.py
@@ -78,12 +78,6 @@ WSGI_APPLICATION = "library_system.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-#     }
-# }
 
 DATABASES = {
     'default': {
@@ -95,7 +89,6 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
